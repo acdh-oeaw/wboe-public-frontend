88630ea35052c696de8e6e519de1e242c74730b1
arnold-graf <arni@recordbird.com>
2018-11-20 12:15:00 +0100

    import L

--- src/components/Maps.vue
+++ src/components/Maps.vue
@@ -15,7 +15,7 @@
 </template>
 <script lang="ts">
   import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
-  import { LMap, LTileLayer, LMarker, LGeoJson } from 'vue2-leaflet'
+  import L, { LMap, LTileLayer, LMarker, LGeoJson } from 'vue2-leaflet'
 
   import axios from 'axios';
 
