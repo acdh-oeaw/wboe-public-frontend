a2a0af088696585557bcc51cfb7a2af42edab341
arnold-graf <arni@recordbird.com>
2018-12-08 19:42:05 +0100

    xlsx and json export

--- src/components/Database.vue
+++ src/components/Database.vue
@@ -19,14 +19,14 @@
         select-all
         class="data-table"
         :total-items="pagination.totalItems"
-        rows-per-page-text="Dokumente pro Seite"
+        rows-per-page-text="Pro Seite"
         :rows-per-page-items="[10, 25, 50, 100]"
         :pagination.sync="pagination"
         :headers="headers"
         :loading="loading"
         item-key="id"
         :items="items">
-        <v-flex slot="actions-prepend" align-start>
+        <v-flex slot="actions-prepend">
           <v-tooltip color="ci" top :disabled="selected.length > 0">
             <v-btn
               slot="activator"
@@ -40,6 +40,18 @@
             </v-btn>
             <span>Wählen Sie zuvor Dokumente aus</span>
           </v-tooltip>
+          <v-menu top open-on-hover>
+            <v-btn slot="activator" :disabled="items.length === 0" small flat class="pl-3 pr-3" round color="ci">
+              Export
+            </v-btn>
+            <v-list class="context-menu-list" dense>
+              <v-subheader>
+                <v-icon class="mr-1" small>save_alt</v-icon> Export/Download
+              </v-subheader>
+              <v-list-tile @click="saveXLSX">Microsoft Excel</v-list-tile>
+              <v-list-tile @click="saveJSON">JSON</v-list-tile>
+            </v-list>
+          </v-menu>
         </v-flex>
         <template slot="items" slot-scope="props">
           <td>
@@ -59,6 +71,8 @@
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { getDocuments, searchDocuments } from '../api'
+import * as FileSaver from 'file-saver'
+import * as xlsx from 'xlsx'
 import * as _ from 'lodash'
 
 @Component
@@ -71,7 +85,7 @@ export default class Database extends Vue {
   pagination = {
     descending: true,
     page: 1,
-    rowsPerPage: 50,
+    rowsPerPage: 100,
     sortBy: null,
     totalItems: 0
   }
@@ -129,6 +143,20 @@ export default class Database extends Vue {
       this.init()
     }
   }
+
+  saveXLSX() {
+    const x = xlsx.utils.json_to_sheet(this.items)
+    const y = xlsx.writeFile({
+      Sheets: { sheet: x },
+      SheetNames: ['sheet'],
+    }, 'text.xlsx')
+  }
+
+  saveJSON() {
+    const blob = JSON.stringify(this.items, undefined, 2)
+    FileSaver.saveAs(new Blob([blob]), 'lioe-database.json')
+  }
+
 }
 </script>
 <style lang="scss">
