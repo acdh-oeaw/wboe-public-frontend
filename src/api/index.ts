2866492d1972aa2c31c3871356519154d2ca5e6c
arnold-graf <arni@recordbird.com>
2018-12-08 18:42:48 +0100

    Documents type, search query

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,6 +6,21 @@ declare var process: {
   }
 }
 
+interface Documents {
+  total: number
+  documents: Array<{
+    Bedeutung: string
+    Fragebogennummer: string
+    Hauptlemma: string
+    Kontext: string
+    Lautung: string
+    Nebenlemma: string
+    Wortart: string
+    entry: any
+    id: string
+  }>
+}
+
 const esEndpoint = 'https://walk-want-grew.acdh.oeaw.ac.at/_search'
 const apiEndpoint = 'https://dboeannotation.acdh-dev.oeaw.ac.at/api'
 const txtEndpoint = 'https://vawadioe.acdh.oeaw.ac.at/lioetxt/'
@@ -28,7 +43,7 @@ export function isExternUrl(url: string): boolean {
   return txtEndpoint !== url.substr(0, txtEndpoint.length)
 }
 
-export async function getDocuments(page = 1, items = 100): Promise<any[]> {
+export async function getDocuments(page = 1, items = 100): Promise<Documents> {
   const r = await (await fetch(apiEndpoint + '/documents/?page=' + page + '&page_size=' + items)).json()
   const ds = (await axios({
     method: 'POST',
@@ -45,10 +60,54 @@ export async function getDocuments(page = 1, items = 100): Promise<any[]> {
     url: localEndpoint + '/es-query'
   })).data
   console.log(ds.hits.hits)
-  return ds.hits.hits
+  return {
+    documents: ds.hits.hits.map((h: any) => ({ ...h._source, id: h._id})),
+    total: ds.hits.total
+  }
 }
 
-export async function getDocumentsByCollection(id: number): Promise<XMLDocument[]> {
+export async function searchDocuments(
+  search: string, page = 1, items = 100, descending = false, sortBy = null
+): Promise<Documents> {
+  console.log(sortBy)
+  const sort = sortBy !== null && {
+    sort: [
+      {
+        [ sortBy! ]: { order: descending ? 'desc' : 'asc' }
+      }
+    ]
+  }
+  console.log(sort)
+  const ds = (await axios(localEndpoint + '/es-query', {
+    method: 'POST',
+    data: {
+      ...sort,
+      from: (page - 1) * items,
+      size: items,
+      query: {
+        multi_match: {
+          fields: [
+            'Hauptlemma',
+            'Bedeutung',
+            'Fragebogennummer',
+            'Kontext',
+            'Lautung',
+            'Nebenlemma',
+            'Wortart'
+          ],
+          query: search,
+          type: 'phrase_prefix'
+        }
+      }
+    }
+  })).data
+  return {
+    documents: ds.hits.hits.map((h: any) => ({ ...h._source, id: h._id})),
+    total: ds.hits.total
+  }
+}
+
+export async function getDocumentsByCollection(id: number): Promise<Documents> {
   const r = await (await fetch(apiEndpoint + '/documents/?in_collections=28')).json()
   console.log(r)
   const ds = await (await fetch(esEndpoint, {
@@ -61,13 +120,16 @@ export async function getDocumentsByCollection(id: number): Promise<XMLDocument[
       }
     })
   })).json()
-  return ds.hits.hits
+  return {
+    documents: ds.hits.hits.map((h: any) => h._source),
+    total: ds.hits.total
+  }
 }
 
 export async function getArticles(search = 'f'): Promise<Array<{ title: string, filename: string }>> {
   // tslint:disable-next-line:max-line-length
   const r = await (await fetch(articleEndpoint + '?initial=' + search)).json()
-  return r.results.article
+  return r.results.article.length ? r.results.article : [ r.results.article ]
 }
 
 export async function getArticleByFileName(fileName: string): Promise<string> {
