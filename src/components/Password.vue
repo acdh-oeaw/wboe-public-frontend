3f24fd8aeba1206272d8debbb63ddef7cbd16557
hcb79 <hcb@hcb.de>
2019-06-05 21:47:55 +0200

    "Hinweise zum Forschungsdatenmanagement" auf Passwort Seite hinzugefügt.

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -2,8 +2,8 @@
   <div class="pa-5 text-xs-center">
     <h1 class="text-light">Zugriffsgeschützter Bereich</h1>
     <p class="grey--text">
-      Bitte geben Sie das Ihnen zugewiesene Passwort an.
-      <br>
+      Bitte geben Sie das Ihnen zugewiesene Passwort an.<br>
+      Hinweise zu unserem Forschungsdatenmanagement finden Sie <a href="https://vawadioe.acdh.oeaw.ac.at/lioetxt/wboe-artikel/hinweise-zum-forschungsdatenmanagement/" target="_blank" @click.prevent="subDialog = true">hier</a>.<br>
       Bei Fragen wenden sie sich bitte an <a href="mailto:philipp.stoeckle@oeaw.ac.at">Dr. Philipp Stöckle</a>.
     </p>
     <v-form @submit.prevent="submit">
@@ -21,16 +21,28 @@
       <h4 class="headline mb-0">Sie suchen das WBÖ-Projekt?</h4>
       <div>Alle Informationen zum „Wörterbuch der bairischen Mundarten in Österreich“ finden Sie <a href="https://vawadioe.acdh.oeaw.ac.at/projekte/wboe/wboe-startseite/" target="_blank">hier</a>.</div>
     </InfoBox>
+    <v-dialog v-model="subDialog" max-width="1000" color="#2b2735" scrollable>
+      <v-card flat class="fill-height pl-4 pt-4 pr-4 pb-3">
+        <div class="close-btn">
+          <v-btn @click="subDialog = false" flat icon><v-icon dark>close</v-icon></v-btn>
+        </div>
+        <v-card-text class="pa-0 fill-height">
+          <info-text class="pa-4 white fill-height" path="wboe-artikel/hinweise-zum-forschungsdatenmanagement/" />
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
 import { userStore } from '../store/user'
+import InfoText from '@components/InfoText.vue'
 import InfoBox from '@components/InfoBox.vue'
 
 @Component({
   components: {
-    InfoBox
+    InfoBox,
+    InfoText
   }
 })
 export default class Password extends Vue {
@@ -41,6 +53,7 @@ export default class Password extends Vue {
   aospdqweumkyxclkqwe: string|null = null
   wrongPassword = false
   userStore = userStore
+  subDialog = false
   submit() {
     if (this.aospdqweumkyxclkqwe !== null && (btoa(this.aospdqweumkyxclkqwe) === this.adijweoqeoqkdkwoqkk || btoa(this.aospdqweumkyxclkqwe) === this.adiuieuenslkfiwensd)) {
       this.userStore.isLoggedIn = true
